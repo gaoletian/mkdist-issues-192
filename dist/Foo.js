44914f9a98be4a776126804a4979c59e452e1800
高乐天 <67890211@qq.com>
2024-01-09 14:24:35 +0000

    first commit

---
+++ dist/Foo.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+module.exports = exports.Type = void 0;
+var Type = exports.Type = /* @__PURE__ */(Type2 => {
+  Type2[Type2["Foo"] = 0] = "Foo";
+  Type2[Type2["Bar"] = 1] = "Bar";
+  return Type2;
+})(Type || {});
+class Action {}
+module.exports = Action;
---
+++ src/Foo.ts
@@ -0,0 +1,6 @@
+export enum Type {
+    Foo,
+    Bar
+}
+
+export default class Action {}
